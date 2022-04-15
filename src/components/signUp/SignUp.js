d54d77863cdc68bbee9386f89ec5e046d09fd499
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-04-16 01:04:40 +0600

    form designed

--- src/components/signUp/SignUp.js
+++ src/components/signUp/SignUp.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import './SignUp.css';
 import { Button, Form } from 'react-bootstrap';
 import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
 import auth from '../../firebaseConfig'
@@ -7,27 +8,41 @@ import auth from '../../firebaseConfig'
 const SignUp = () => {
     const [signInWithGoogle, user] = useSignInWithGoogle(auth)
     return (
-        <div>
-            <Form className='w-50 mx-auto mt-5'>
+        <div className=' user-form'>
+            <Form >
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Label className='text-light mb-0'>Name</Form.Label>
+                    <Form.Control className='input-fild' placeholder='Name' type="text"  />
                     <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
+                        
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label className='text-light mb-0'>Email</Form.Label>
+                    <Form.Control className='' placeholder='Email' type="email"  />
+                    <Form.Text className="text-muted">
+                        
                     </Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" autoComplete='false' />
+                    <Form.Label className='text-light mb-0'>Password</Form.Label>
+                    <Form.Control type="password" placeholder='Password'  autoComplete='false' />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Sign Up
-                </Button>
+                <div className='text-center mb-3'>
+                    <Button className='submit-btn' type="submit"> Sign Up </Button>
+               </div>
             </Form>
-            <Button onClick={()=>signInWithGoogle()} variant="primary" type="submit">
+            <div className='d-flex justify-content-evenly text-light'>
+                <hr className='line' />
+                <p>Or</p>
+                <hr className=' line' />
+            </div>
+            <div className="text-center">
+                <Button onClick={()=>signInWithGoogle()} className='' variant="primary" type="submit">
                     Google SignIn
-            </Button>
+                </Button>
+            </div>
         </div>
     );
 };
